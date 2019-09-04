0752f6b4e17819b4f87c5ed96e69542a47fc2ae0
Nofrets Poai <opetstudio@gmail.com>
2019-09-04 19:30:29 +0700

    generate form for submit mercahnt

--- src/Components/Merchant/MerchantCreatePageComponent.js
+++ src/Components/Merchant/MerchantCreatePageComponent.js
@@ -21,58 +21,122 @@ class MerchantCreatePageComponent extends Component {
     return (
       <div className='content-wrapper'>
         <section className='content-header'>
-          <h1>
-            Merchant Create
-          </h1>
+          <h1>Merchant Create</h1>
           <ol className='breadcrumb'>
-            <li><a href='#'><i className='fa fa-dashboard' /> Merchant</a></li>
+            <li>
+              <a href='#'>
+                <i className='fa fa-dashboard' /> Merchant
+              </a>
+            </li>
             <li className='active'>Create</li>
           </ol>
         </section>
         <section className='content'>
-          <form onSubmit={(e) => this._onSubmitForm(e)}>
+          <form onSubmit={e => this._onSubmitForm(e)}>
             <div className='box box-primary'>
               <div className='box-header with-border'>
                 <h3 className='box-title'>Merchant Information</h3>
               </div>
               <div className='box-body'>
+                {this.props.responseCode !== '' &&
+                  this.props.responseCode === 'MBDD00' && (
+                    <div className='row'>
+                      <div className='col-md-12'>
+                        <div class='alert alert-success' role='alert'>
+                          {this.props.responseMessage}
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                {this.props.responseCode !== '' &&
+                  this.props.responseCode !== 'MBDD00' && (
+                    <div className='row'>
+                      <div className='col-md-12'>
+                        <div class='alert alert-danger' role='alert'>
+                          {this.props.responseMessage}
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 <div className='row'>
-                  
                   <div className='col-md-6'>
-
                     <div className='box-body'>
                       <div className='form-group'>
                         <label for='exampleInputEmail1'>Merchant Code</label>
-                        <input type='mcode' className='form-control' id='mcode' placeholder='Enter Merchant Code' ref='mcode' />
+                        <input
+                          type='mcode'
+                          className='form-control'
+                          id='mcode'
+                          placeholder='Enter Merchant Code'
+                          ref='mcode'
+                        />
                       </div>
                       <div className='form-group'>
                         <label for='exampleInputPassword1'>Merchant Name</label>
-                        <input type='mname' className='form-control' id='mname' placeholder='Enter Merchant Name' ref='mname' />
+                        <input
+                          type='mname'
+                          className='form-control'
+                          id='mname'
+                          placeholder='Enter Merchant Name'
+                          ref='mname'
+                        />
                       </div>
                       <div className='form-group'>
                         <label for='exampleInputEmail1'>Email address</label>
-                        <input type='email' className='form-control' id='email' placeholder='Enter email' ref='email' />
+                        <input
+                          type='email'
+                          className='form-control'
+                          id='email'
+                          placeholder='Enter email'
+                          ref='email'
+                        />
                       </div>
                       <div className='form-group'>
                         <label for='exampleInputPassword1'>Phone No.</label>
-                        <input type='phone' className='form-control' id='phone' placeholder='Enter Phone Number' ref='phone' />
+                        <input
+                          type='phone'
+                          className='form-control'
+                          id='phone'
+                          placeholder='Enter Phone Number'
+                          ref='phone'
+                        />
                       </div>
                       <div className='form-group'>
                         <label>Address</label>
-                        <textarea className='form-control' rows='3' placeholder='Enter Address' ref='address' />
+                        <textarea
+                          className='form-control'
+                          rows='3'
+                          placeholder='Enter Address'
+                          ref='address'
+                        />
                       </div>
                     </div>
-
                   </div>
                   <div className='col-md-6'>
                     <div className='box-body'>
                       <div className='form-group'>
-                        <label for='exampleInputEmail1'>Front End Callback URL</label>
-                        <input type='frontend' className='form-control' id='frontend' placeholder='Enter Front End Callback URL' ref='frontend' />
+                        <label for='exampleInputEmail1'>
+                          Front End Callback URL
+                        </label>
+                        <input
+                          type='frontend'
+                          className='form-control'
+                          id='frontend'
+                          placeholder='Enter Front End Callback URL'
+                          ref='frontend'
+                        />
                       </div>
                       <div className='form-group'>
-                        <label for='exampleInputPassword1'>Back End Callback URL</label>
-                        <input type='backend' className='form-control' id='backend' placeholder='Enter Back End Callback URL' ref='backend' />
+                        <label for='exampleInputPassword1'>
+                          Back End Callback URL
+                        </label>
+                        <input
+                          type='backend'
+                          className='form-control'
+                          id='backend'
+                          placeholder='Enter Back End Callback URL'
+                          ref='backend'
+                        />
                       </div>
                       <div className='form-group'>
                         <label>Status</label>
@@ -83,17 +147,17 @@ class MerchantCreatePageComponent extends Component {
                       </div>
                     </div>
                   </div>
-                
                 </div>
               </div>
               <div className='box-footer'>
-                <button type='submit' className='btn btn-primary'>Submit</button>
+                <button type='submit' className='btn btn-primary'>
+                  Submit
+                </button>
                 {/* <button type="submit" className="btn btn-info pull-right">Sign in</button> */}
               </div>
             </div>
           </form>
         </section>
-
       </div>
     )
   }
