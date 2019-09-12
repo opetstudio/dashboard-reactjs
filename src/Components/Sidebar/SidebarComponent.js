b9f29e87eb3d7d23daebffc072999cd0e7dd4544
Nofrets Poai <opetstudio@gmail.com>
2019-09-12 16:33:22 +0700

    adding menus

--- src/Components/Sidebar/SidebarComponent.js
+++ src/Components/Sidebar/SidebarComponent.js
@@ -20,6 +20,18 @@ class SidebarComponent extends Component {
           </div>
           <ul className='sidebar-menu' data-widget='tree'>
             <li className='header'>MAIN NAVIGATION</li>
+            <li className='treeview'>
+              <Link to='#'>
+                <i className='fa fa-dashboard' /> <span>Payment Gateway</span>
+                <span className='pull-right-container'>
+                  <i className='fa fa-angle-left pull-right' />
+                </span>
+              </Link>
+              <ul className='treeview-menu'>
+                <li><Link to='/merchant/create'><i className='fa fa-circle-o' /> Create New</Link></li>
+                <li className='active'><Link to='/merchant/list'><i className='fa fa-circle-o' /> All PG</Link></li>
+              </ul>
+            </li>
             <li className='active treeview menu-open'>
               <Link to='#'>
                 <i className='fa fa-dashboard' /> <span>Merchant</span>
@@ -33,7 +45,19 @@ class SidebarComponent extends Component {
                 <li><Link to='/merchant/change-limit'><i className='fa fa-circle-o' /> Change Limit</Link></li>
               </ul>
             </li>
-
+            <li className='treeview'>
+              <Link to='#'>
+                <i className='fa fa-dashboard' /> <span>User</span>
+                <span className='pull-right-container'>
+                  <i className='fa fa-angle-left pull-right' />
+                </span>
+              </Link>
+              <ul className='treeview-menu'>
+                <li><Link to='/merchant/create'><i className='fa fa-circle-o' /> Create New</Link></li>
+                <li className='active'><Link to='/merchant/list'><i className='fa fa-circle-o' /> All User</Link></li>
+                <li><Link to='/merchant/change-limit'><i className='fa fa-circle-o' /> Change Limit</Link></li>
+              </ul>
+            </li>
             <li className='treeview'>
               <Link to='#'>
                 <i className='fa fa-pie-chart' />
@@ -44,6 +68,19 @@ class SidebarComponent extends Component {
               </Link>
               <ul className='treeview-menu'>
                 <li><Link to='/report'><i className='fa fa-circle-o' /> Report</Link></li>
+              </ul>
+            </li>
+            <li className='treeview'>
+              <Link to='#'>
+                <i className='fa fa-dashboard' /> <span>Profile</span>
+                <span className='pull-right-container'>
+                  <i className='fa fa-angle-left pull-right' />
+                </span>
+              </Link>
+              <ul className='treeview-menu'>
+                <li><Link to='/merchant/create'><i className='fa fa-circle-o' /> Profile Merchant</Link></li>
+                <li className='active'><Link to='/merchant/list'><i className='fa fa-circle-o' /> Profile PG</Link></li>
+                <li><Link to='/merchant/list'><i className='fa fa-circle-o' /> Profile User</Link></li>
               </ul>
             </li>
           </ul>
